0cd38aa2682eb7ccfdf5453879f3b035e3c09387
Reagin Turner <rturnerafteragnes@gmail.com>
2021-09-22 10:50:39 -0400

    Fixing the last question to get to the end of the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,26 +11,43 @@ var results = document.querySelector("#results")
 //Question and answer strings
 var quiz = {
 questions: ["ques1", "ques2", "ques3", "ques4", "ques5"],
-answer1: ["1234", "5678 ", "66 ", " 77", " 88"],
-answer2: ["33 ", " 55", " 66", "77 ", " 88"],
-answer3: [" 44", "55 ", " 66", "77 ", " 99"],
-answer4: ["55 ", "55 ", " 66", "77 ", " 99"],
-correctAnswers: [" 44", "5678", " 66", " 77", " 99"],
+answer1: ["1", "2", "3", "4", "5"],
+answer2: ["11", "22", "33", "44", "55"],
+answer3: ["111", "222", "333", "444", "555"],
+answer4: ["1111", "2222", "3333", "4444", "5555"],
+correctAnswers: ["111", "2", "3333", "4", "555"],
 }
 
 i = 1
 
 //buttons for the answers 
-listItems.addEventListener("click", function(){
-    var answer = event.target.textContent
-    // target = correctAnswers[1];
-    if(answer == quiz.correctAnswers[i]){ //fix if statement
-        results.textContent = "Correct!"
+listItems.addEventListener("click", function(event){
+    var answer = event.target.textContent;
+    // console.dir(event.target.textContent);
+    if(answer === quiz.correctAnswers[i]){ //fix if statement
+        results.textContent = "Correct!";
     }
-    else if(secondsLeft > 0){
+    else if(answer !== quiz.correctAnswers[i] && secondsLeft > 0){
         results.textContent = "Wrong...";
-        secondLeft = secondsLeft - 5;
+        secondsLeft = secondsLeft - 5;
     }
+    else if(answer !== quiz.correctAnswers[i] && secondsLeft <= 0){
+        clearInterval(timerItvl);
+          // Calls function to end quiz and display score
+        endQuiz();
+    } 
+    else if (answer === quiz.correctAnswers[4] && secondsLeft > 0){
+        endQuiz();
+    }   
+    
+    else {
+        endQuiz();
+    }
+        //             question.textContent = "You scored " + secondsLeft;
+        //         } else {
+        //             secondsLeft = secondsLeft - 5;
+        //             question.textContent = "You scored " + secondsLeft;
+        //             }
 
     if (i < quiz.questions.length){
         questionEl.textContent = quiz.questions[i];
@@ -39,72 +56,14 @@ listItems.addEventListener("click", function(){
         ans3.textContent = quiz.answer3[i];
         ans4.textContent = quiz.answer4[i];
         i++;
-    console.log("button shoud go to next qs")
+        console.log("button shoud go to next qs")
     }  
 
+    if(i === (quiz.questions[4])){
+        endQuiz();
+    }
+
     });
-// //Question #2
-//     function question2(){
-//         question.textContent = quiz.questions[1];
-//         ans1.textContent = quiz.answer1[1];
-//         ans2.textContent = quiz.answer2[1];
-//         ans3.textContent = quiz.answer3[1];
-//         ans4.textContent = quiz.answer4[1];
-
-//         if (quiz.correctAnswers[1] === quiz.answer1[1] && secondsLeft!== 0){
-//             question3();
-//         } else {
-//             secondsLeft = secondsLeft - 5;
-//             question3();
-//             }
-//     }
-// //Question #3
-//     function question3(){
-//         question.textContent = quiz.questions[2];
-//         ans1.textContent = quiz.answer1[2];
-//         ans2.textContent = quiz.answer2[2];
-//         ans3.textContent = quiz.answer3[2];
-//         ans4.textContent = quiz.answer4[2];
-
-//         if (quiz.correctAnswers[2] === quiz.answer4[2] && secondsLeft!== 0){
-           
-//             question4();
-//         } else {
-//             secondsLeft = secondsLeft - 5;
-//             question4();
-//             }
-//     }
-// //Question #4
-//     function question4(){
-//         question.textContent = quiz.questions[3];
-//         ans1.textContent = quiz.answer1[3];
-//         ans2.textContent = quiz.answer2[3];
-//         ans3.textContent = quiz.answer3[3];
-//         ans4.textContent = quiz.answer4[3];
-
-//         if (quiz.correctAnswers[3] === quiz.answer1[3] && secondsLeft!== 0){
-//             question5();
-//         } else {
-//             secondsLeft = secondsLeft - 5;
-//             question5();
-//             }
-//     }
-// //Question #5
-//     function question5(){
-//         question.textContent = quiz.questions[4];
-//         ans1.textContent = quiz.answer1[4];
-//         ans2.textContent = quiz.answer2[4];
-//         ans3.textContent = quiz.answer3[4];
-//         ans4.textContent = quiz.answer4[4];
-
-//         if (quiz.correctAnswers[4] === quiz.answer3[4] && secondsLeft!== 0){
-            
-//             question.textContent = "You scored " + secondsLeft;
-//         } else {
-//             secondsLeft = secondsLeft - 5;
-//             question.textContent = "You scored " + secondsLeft;
-//             }
-//     }
 
 
 // start button function
@@ -113,13 +72,13 @@ startBtn.addEventListener("click", function(){
     document.querySelector(".start-text").style.display = "none";
     document.querySelector("#instructions").style.display = "none";
     listItems.style.visibility = "visible";
+    
     questionEl.textContent = quiz.questions[0];
     ans1.textContent = quiz.answer1[0];
     ans2.textContent = quiz.answer2[0];
     ans3.textContent = quiz.answer3[0];
     ans4.textContent = quiz.answer4[0];
 
-    //name of function for eventlistener
 
 })
 
@@ -155,8 +114,14 @@ var startTimer = function() {
 
 function endQuiz(){
     timer.textContent = "Time's Up!"
+    question.textContent = "You scored " + secondsLeft;
     //getScore();
     //Clear the questions and answers
+    
+    ans1.textContent.setAttribute("style", "display:none;")
+    ans2.textContent.setAttribute("style", "display:none;") 
+    ans3.textContent.setAttribute("style", "display:none;")
+    ans4.textContent.setAttribute("style", "display:none;")
 }
 
 // Add highscore info
